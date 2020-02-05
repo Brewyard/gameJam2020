cd07e36394b7428020eaea6f3b742bef5c895365
Vic38 <46890625+Vic38@users.noreply.github.com>
2020-02-05 10:12:34 +0100

    ajout fichier exemple animation bulle

--- Model/game.py
+++ Model/game.py
@@ -1,22 +1,19 @@
-from player import Player
 import pygame
+from player import Player
+
 Texty = pygame.font.Font('../Images/SUPERPOI_R.TTF', 20)
 Textyy = pygame.font.Font('../Images/SUPERPOI_R.TTF', 10)
 
-import pygame
 class Game():
     def __init__(self):
         self.player = Player()
         self.pressed = {
-            "touche fleche droite" : True,
+            "touche fleche droite": True,
             "touche fleche gauche": False,
-            "touche fleche space" : False
+            "touche fleche space": False
         }
 
-
-
-    def jeu(self,screen,rect):
-       # imageMenu = pygame.image.load("../Images/background_Menu.jpg")
-       # screen.blit(imageMenu, rect)
+    def jeu(self, screen, rect):
+        # imageMenu = pygame.image.load("../Images/background_Menu.jpg")
+        # screen.blit(imageMenu, rect)
         pass
-
