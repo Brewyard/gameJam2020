88b9df7c05094093c2954da3967fb51799fb2a45
brewyard <to.38@live.fr>
2020-02-06 22:47:36 +0100

    game over pas mal manque le retour à un nouveau jeu avec reset, on demande nom du joueur que quand il bat le record

--- Model/main.py
+++ Model/main.py
@@ -65,7 +65,7 @@ imagesVent = [load_image(PATH + "vent1.png"), load_image(PATH + "vent2.png"), lo
 # except:
 #     print('nani')
 # imagesVent = [load_image(PATH + "wind-sens-1.png"), load_image(PATH + "wind-sens-2.png"), load_image(PATH + "wind-sens-1.png")]
-#imagesVent = [load_image(PATH + "wind1.png"), load_image(PATH + "wind2.png"), load_image(PATH + "wind3.png"), load_image(PATH + "wind4.png")]
+# imagesVent = [load_image(PATH + "wind1.png"), load_image(PATH + "wind2.png"), load_image(PATH + "wind3.png"), load_image(PATH + "wind4.png")]
 movingBackground = MovingBackground(imagesVent)
 obstacle2 = movingBackground.generateObstacles()
 game.all_sprites.add(obstacle2)
@@ -73,50 +73,53 @@ game.all_sprites.add(obstacle2)
 # game.menu(screen)
 score = 0
 
-def text1(word,x,y):
-    #Generer la fenetre de notre jeu
-    screen = pygame.display.set_mode(windowSize)
-    font = pygame.font.SysFont(None, 25)
-    text = font.render("{}".format(word), True, (255,0,0))
-    return screen.blit(text,(x,y))
+
+def text1(word, x, y, sizeFont):
+    # Generer la fenetre de notre jeu
+    # screen = pygame.display.set_mode(windowSize)
+    font = pygame.font.Font('../Fonts/gumbonormal.ttf', sizeFont)
+    text = font.render("{}".format(word), True, (0, 153, 255))
+    return screen.blit(text, (x, y))
+
 
 def inpt():
-    word=""
-    text1("Please enter your name: ", 300, 400) #example asking name
+    screen.fill((0, 0, 0))
+    word = ""
+    text1("Please enter your name: ", 100, 200, 40)  # example asking name
     pygame.display.flip()
     done = True
     while done:
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    word+=str(chr(event.key))
+                    word += str(chr(event.key))
                 if event.key == pygame.K_b:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_c:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_d:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_e:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_f:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_g:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_h:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_i:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_j:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_k:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_l:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_m:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_n:
                     word += chr(event.key)
                 if event.key == pygame.K_o:
@@ -124,35 +127,36 @@ def inpt():
                 if event.key == pygame.K_p:
                     word += chr(event.key)
                 if event.key == pygame.K_q:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_r:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_s:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_t:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_u:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_v:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_w:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_x:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_y:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_z:
-                    word+=chr(event.key)
+                    word += chr(event.key)
                 if event.key == pygame.K_RETURN:
-                    done=False
-                print(word)
-                text1(word, 300, 500)
+                    done = False
+                text1("Please enter your name: ", 100, 200, 40)
+                text1(word, 300, 400, 20)
                 pygame.display.flip()
     return word
 
+
 def gameOver():
-    Texty = pygame.font.Font('../Fonts/Polo Bubble.ttf', 20)
-    TextChiffre = pygame.font.SysFont('arial', 20)
+    font = pygame.font.Font('../Fonts/gumbonormal.ttf', 20)
+    bigFont = pygame.font.Font('../Fonts/gumbonormal.ttf', 60)
 
     global score, placement, selectorGameOver
     global windowSize, origin, screen
@@ -161,38 +165,38 @@ def gameOver():
     bubble_pop.play()
     game.player.image.fill(transparent)
 
-
-    nameEntre = inpt()
-    f = open('../name.txt', 'w')
-    f.write(nameEntre)
-    f.close()
-
-    f = open('../highscore.txt','r')
+    f = open('../highscore.txt', 'r')
     highscore = int(f.read())
     f.close()
     if score >= highscore:
-        highscore = score
-        f = open('../highscore.txt','w')
-        f.write(str(highscore))
+        # demande le nom
+        nameEntre = inpt()
+        f = open('../name.txt', 'w')
+        f.write(nameEntre)
         f.close()
 
+        highscore = score
+        f = open('../highscore.txt', 'w')
+        f.write(str(highscore))
+        f.close()
 
     windowSize = (800, 600)
     origin = (0, 0)
 
-
     screen = pygame.display.set_mode(windowSize)
     pygame.display.set_caption('Bubble Escape')
-    textMort = Texty.render('Game Over ', 0, (0, 0, 255))
-    textHighscore = TextChiffre.render("Highscore : "+str(highscore),0,(255,0,0))
-    textScore = TextChiffre.render("Score : "+str(score),0,(255,0,0))
-    screen.blit(textMort, (300, 200))
+    textMort = bigFont.render('Game Over ', 0, (255, 0, 0))
+    textHighscore = font.render("Highscore : " + str(highscore), 0, (255, 0, 0))
+    textScore = font.render("Score : " + str(score), 0, (255, 0, 0))
+    screen.blit(textMort, (180, 270))
     screen.blit(textHighscore, (50, 50))
     screen.blit(textScore, (50, 100))
     pygame.display.update()
     score = 0
-    time.sleep(5) #au bout de 5 seconde on revient au menus du choix des niveaux
-    return
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                return
 
 
 # boucle principale
@@ -211,7 +215,6 @@ def playing(difficulte):
 
     compteToursGlobalPourAcceleration = 0
     vitesseBoostEtOiseauEnPlus = 0
-
 
     en_jeu = False
     while launch:
@@ -246,7 +249,6 @@ def playing(difficulte):
             game.all_sprites.add(bird)
 
         movingBackground.fall(game.vitesseAcceleration + game.vitesseBullePercee)
-
 
         # si bulle touche obstacle
         if movingBackground.windTouch(game.player.rect):
@@ -330,7 +332,6 @@ def playing(difficulte):
             if ilPeut:
                 game.player.move_y(-10)
 
-
         if rect.inflate(150, 150).contains(game.player.rect):
             score += 1
             riendutout = 0
@@ -340,7 +341,6 @@ def playing(difficulte):
             game.resetGame()
             en_jeu = False
             return en_jeu
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
